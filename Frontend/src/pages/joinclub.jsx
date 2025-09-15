60d97e9dff5216c523b5353d3607813260e7a2f8
Vikash <yadav621gzp@gmail.com>
2025-09-15 23:30:17 +0530

    Update joinclub.jsx: Replace Linea Club with AI Treasury DAO and ETH with CFX

--- Frontend/src/pages/joinclub.jsx
+++ Frontend/src/pages/joinclub.jsx
@@ -64,7 +64,7 @@ function JoinClub() {
           <div className="sidebar-brand-icon rotate-n-15">
             <i className="fas fa-laugh-wink" />
           </div>
-        <div className="sidebar-brand-text mx-3">Linea Club</div>
+        <div className="sidebar-brand-text mx-3">🤖 AI Treasury DAO</div>
         </a>
         {/* Divider */}
         <hr className="sidebar-divider my-0" />
@@ -124,7 +124,7 @@ function JoinClub() {
                     <div className="row no-gutters align-items-center">
                       <div className="col mr-2">
                         <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                          Balance (ETH)</div>
+                          Balance (CFX)</div>
                         <div className="h5 mb-0 font-weight-bold text-gray-800 view_balance_address">0</div>
                       </div>
                       <div className="col-auto">
